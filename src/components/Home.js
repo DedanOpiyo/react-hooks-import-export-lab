4a51cc2255cb8d3446fb45b4f431fc17bc498d08
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-04-09 21:32:58 +0300

    exploring imports and exports(of variables) in react.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { username, city } from "../data/user"; // Importing Specific Variables
+import * as userData from "../data/user"; // Importing Multiple Variables
+console.log(userData);
 
 function Home() {
   return (
@@ -6,6 +9,10 @@ function Home() {
       <h1>
         {username} is a Web Developer from {city}
       </h1>
+      <h2>{userData.username}</h2>
+      <h2>from {userData.city}</h2>
     </div>
   );
 }
+
+export default Home;
